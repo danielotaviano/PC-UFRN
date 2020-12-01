b0236a5679f8f2e919b5c1fcca893755df6d1fee
unknown <freitasandre38@gmail.com>
2020-12-01 17:56:36 -0300

    fiz umas paradas aí

--- src/main.py
+++ src/main.py
@@ -1,51 +1,52 @@
 import sys
-from getArgs import getArgs
 from exceptions import raiseExceptions
 from getNameFiles import getNameFiles
-from getNames import getNames
-from writeNames import writeNames
 
-
-sys.argv.remove(sys.argv[0])
-argv = sys.argv
-
-if (len(argv) <= 1):
+def getArgs():
+  sys.argv.remove(sys.argv[0])
+  return sys.argv
+def excep (argv):
+  if (len(argv) <= 1):
     raise Exception("Para executar o programa, passe os argumentos. ex: -i NomeDoArquivoDeOrigem.txt -o NomeDoArquivoDeSaida.txt") 
 
-if ('-i' not in argv):
+  if ('-i' not in argv):
     raise Exception("Para execução do programa, é obrigatório a passagem do arquivo de origem como argumento. ex: -i NomeDoArquivoDeOrigem.txt") 
-
+  
 inputNameFile = ''
 outputNameFile = ''
 
-for i,e in enumerate(argv):
-  if (e == '-i'):
-    inputNameFile = argv[i+1]
-  if (e == '-o'):
-    outputNameFile = argv[i+1]
+def getNamesFiles(argv):
+  nameFile = ''
+  outputNameFile = ''
 
-if (not outputNameFile):
-  outputNameFile = 'sort_{a}'.format(a = inputNameFile)
+  for i,e in enumerate(argv):
+    if (e == '-i'):
+      inputNameFile = argv[i+1]
+    if (e == '-o'):
+      outputNameFile = argv[i+1]
 
-names = []
+  if (not outputNameFile):
+    outputNameFile = 'sort_{nameFile}'.format(nameFile = inputNameFile)
+  return nameFile,outputNameFile
 
-f = open(inputNameFile,'r')
+def getNames(FileName):
+  names = []
+  f = open(FileName,'r',encoding = 'utf8')
 
-for name in f.read().split('\n'):
-  names.append(name)
+  for name in f.read().split('\n'):
+    names.append(name)
+  return names  
 
+def writeNames(names,outputNameFile):
+  fs = open(outputNameFile,'w',encoding = 'utf8')
+  for name in names:
+    fs.write(name + '\n' )
+  fs.close()
+
+args = getArgs()
+excep(args)
+fileName, outputNameFile = getNameFiles(args)
+names = getNames(fileName)
 names.sort()
-
-fs = open(outputNameFile, "w")
-for name in names:
-  fs.write(name + '\n' )
-
-fs.close()
-
-# args = getArgs()
-# raiseExceptions(args)
-# fileName, outputNameFile = getNameFiles(args)
-# names = getNames(fileName)
-# names.sort()
-# writeNames(names,outputNameFile)
+writeNames(names,outputNameFile)
 
